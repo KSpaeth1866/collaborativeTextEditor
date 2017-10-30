9c9dbe8dadfd8e650513196dd51b3d98a56b44ab
natedana <natepdana@gmail.com>
2017-10-30 15:25:52 -0700

    color start

--- reactApp/assets/styles.js
+++ reactApp/assets/styles.js
@@ -2,7 +2,7 @@ const styles = {
   root: {
     fontFamily: '\'Helvetica\', sans-serif',
     padding: 20,
-    width: 600,
+    width: 600
   },
   editor: {
     fontFamily: '\'Helvetica\', sans-serif',
@@ -20,8 +20,31 @@ const styles = {
   },
   button: {
     marginTop: 10,
-    textAlign: 'center',
+    textAlign: 'center'
   },
+  colorStyleMap: {
+    red: {
+      color: 'rgba(255, 0, 0, 1.0)'
+    },
+    orange: {
+      color: 'rgba(255, 127, 0, 1.0)'
+    },
+    yellow: {
+      color: 'rgba(180, 180, 0, 1.0)'
+    },
+    green: {
+      color: 'rgba(0, 180, 0, 1.0)'
+    },
+    blue: {
+      color: 'rgba(0, 0, 255, 1.0)'
+    },
+    indigo: {
+      color: 'rgba(75, 0, 130, 1.0)'
+    },
+    violet: {
+      color: 'rgba(127, 0, 255, 1.0)'
+    }
+  };
 };
 
 export default styles;
--- reactApp/components/Draft.js
+++ reactApp/components/Draft.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  Editor,
-  EditorState,
-  RichUtils,
+Editor,
+EditorState,
+RichUtils,
 } from 'draft-js';
 import styles from '../assets/styles'
 
@@ -53,6 +53,36 @@ class Draft extends React.Component {
     );
   }
 
+  _toggleColor(toggledColor) {
+    const {editorState} = this.state;
+    const selection = editorState.getSelection();
+    // Let's just allow one color at a time. Turn off all active colors.
+    const nextContentState = Object.keys(colorStyleMap)
+    .reduce((contentState, color) => {
+      return Modifier.removeInlineStyle(contentState, selection, color)
+    }, editorState.getCurrentContent());
+    let nextEditorState = EditorState.push(
+      editorState,
+      nextContentState,
+      'change-inline-style'
+    );
+    const currentStyle = editorState.getCurrentInlineStyle();
+    // Unset style override for current color.
+    if (selection.isCollapsed()) {
+      nextEditorState = currentStyle.reduce((state, color) => {
+        return RichUtils.toggleInlineStyle(state, color);
+      }, nextEditorState);
+    }
+    // If the color is being toggled on, apply it.
+    if (!currentStyle.has(toggledColor)) {
+      nextEditorState = RichUtils.toggleInlineStyle(
+        nextEditorState,
+        toggledColor
+      );
+    }
+    this.onChange(nextEditorState);
+  }
+
   render() {
     return (
       <div className={'RichEditor-root'}>
@@ -87,109 +117,133 @@ class Draft extends React.Component {
             />
           </div>
         </div>
-      );
-    }
-  };
+      )};
+};
 
-  /*********************
-  copy pasta
-  *********************/
+/*********************
+copy pasta
+*********************/
 
-  class StyleButton extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-
-    onToggle(e) {
-      e.preventDefault();
-      this.props.onToggle(this.props.style);
-    };
-
-    render() {
-      let className = 'RichEditor-styleButton';
-      if (this.props.active) {
-        className += ' RichEditor-activeButton';
-      }
-      return (
-        <span
-          className={className}
-          onMouseDown={(e) => this.onToggle(e)}
-          >
-          {this.props.label}
-        </span>
-      );
-    }
+class StyleButton extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  const BLOCK_TYPES = [
-    {label: 'H1', style: 'header-one'},
-    {label: 'H2', style: 'header-two'},
-    {label: 'H3', style: 'header-three'},
-    {label: 'H4', style: 'header-four'},
-    {label: 'H5', style: 'header-five'},
-    {label: 'H6', style: 'header-six'},
-    {label: 'Blockquote', style: 'blockquote'},
-    {label: 'UL', style: 'unordered-list-item'},
-    {label: 'OL', style: 'ordered-list-item'},
-    {label: 'Code Block', style: 'code-block'},
-  ];
-
-  class BlockStyleControls extends React.Component{
-    constructor(props) {
-      super(props)
-
-      this.selection = this.props.editorState.getSelection();
-      this.blockType = this.props.editorState
-      .getCurrentContent()
-      .getBlockForKey(this.selection.getStartKey())
-      .getType();
-    }
-
-    render() {
-      return (
-        <div className="RichEditor-controls">
-          {BLOCK_TYPES.map((type) =>
-            <StyleButton
-              key={type.label}
-              active={type.style === this.blockType}
-              label={type.label}
-              onToggle={(style) => this.callToggle(style)}
-              onToggle={(style) => this.props.onToggle(style)}
-              style={type.style}
-            />
-          )}
-        </div>
-      );
-    }
+  onToggle(e) {
+    e.preventDefault();
+    this.props.onToggle(this.props.style);
   };
 
-  var INLINE_STYLES = [
-    {label: 'Bold', style: 'BOLD'},
-    {label: 'Italic', style: 'ITALIC'},
-    {label: 'Underline', style: 'UNDERLINE'},
-    {label: 'Monospace', style: 'CODE'},
-  ];
-
-  class InlineStyleControls extends React.Component {
-    constructor(props) {
-      super(props)
-      this.currentStyle = this.props.editorState.getCurrentInlineStyle();
+  render() {
+    let className = 'RichEditor-styleButton';
+    if (this.props.active) {
+      className += ' RichEditor-activeButton';
     }
-    render() {
-      return (
-        <div className="RichEditor-controls">
-          {INLINE_STYLES.map(type =>
-            <StyleButton
-              key={type.label}
-              active={this.currentStyle.has(type.style)}
-              label={type.label}
-              onToggle={(style) => this.props.onToggle(style)}
-              style={type.style}
-            />
-          )}
-        </div>
-      );
-    }
-  };
+    return (
+      <span
+        className={className}
+        onMouseDown={(e) => this.onToggle(e)}
+        >
+        {this.props.label}
+      </span>
+    );
+  }
+}
 
-  export default Draft;
+const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one'},
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'H4', style: 'header-four'},
+  {label: 'H5', style: 'header-five'},
+  {label: 'H6', style: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Code Block', style: 'code-block'},
+];
+
+class BlockStyleControls extends React.Component{
+  constructor(props) {
+    super(props)
+
+    this.selection = this.props.editorState.getSelection();
+    this.blockType = this.props.editorState
+    .getCurrentContent()
+    .getBlockForKey(this.selection.getStartKey())
+    .getType();
+  }
+
+  render() {
+    return (
+      <div className="RichEditor-controls">
+        {BLOCK_TYPES.map((type) =>
+          <StyleButton
+            key={type.label}
+            active={type.style === this.blockType}
+            label={type.label}
+            onToggle={(style) => this.callToggle(style)}
+            onToggle={(style) => this.props.onToggle(style)}
+            style={type.style}
+          />
+        )}
+      </div>
+    );
+  }
+};
+
+var INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Underline', style: 'UNDERLINE'},
+  {label: 'Monospace', style: 'CODE'},
+];
+
+class InlineStyleControls extends React.Component {
+  constructor(props) {
+    super(props)
+    this.currentStyle = this.props.editorState.getCurrentInlineStyle();
+  }
+  render() {
+    return (
+      <div className="RichEditor-controls">
+        {INLINE_STYLES.map(type =>
+          <StyleButton
+            key={type.label}
+            active={this.currentStyle.has(type.style)}
+            label={type.label}
+            onToggle={(style) => this.props.onToggle(style)}
+            style={type.style}
+          />
+        )}
+      </div>
+    );
+  }
+};
+
+var COLORS = [
+  {label: 'Red', style: 'red'},
+  {label: 'Orange', style: 'orange'},
+  {label: 'Yellow', style: 'yellow'},
+  {label: 'Green', style: 'green'},
+  {label: 'Blue', style: 'blue'},
+  {label: 'Indigo', style: 'indigo'},
+  {label: 'Violet', style: 'violet'},
+];
+const ColorControls = (props) => {
+  var currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div style={styles.controls}>
+      {COLORS.map(type =>
+        <StyleButton
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Draft;
